071de0209c86cadf74014fb501571b38e6c07a99
Khue Doan <khuedoan98@gmail.com>
2019-07-31 14:44:03 +0700

    Now can read all log files in folder

--- kpi_info/dashboard/management/commands/updatedb.py
+++ kpi_info/dashboard/management/commands/updatedb.py
@@ -3,15 +3,22 @@ from ...models import Revenue
 from django.db.models import Sum
 import csv
 import datetime
-
+import glob
 
 class Command(BaseCommand):
     help = 'Parse data from CSV log file'
 
     def handle(self, *args, **options):
-        logfile = open('data/nikki/20190701/2019-06-02/nikkisea_datalog_chargelog_hourly/Nikki_Logcharge_2019-06-01-00.csv', encoding="latin-1")
-        log = csv.DictReader(logfile)
+        log_dir = 'data/nikki/*/*/nikkisea_datalog_chargelog_hourly/*.csv'
+        for log in glob.iglob(log_dir):
+            self.write_log_to_db(log)
 
+    def readlog(self, log_path):
+        logfile = open(log_path, encoding='latin-1')
+        return csv.DictReader(logfile)
+
+    def write_log_to_db(self, log_path):
+        log = self.readlog(log_path)
         revenue = Revenue()
 
         for row in log:
@@ -20,7 +27,3 @@ class Command(BaseCommand):
             datetime_from_transid = revenue.id[:revenue.id.find("-")]
             revenue.datetime = datetime.datetime.strptime(datetime_from_transid, '%Y%m%d%H%M%S')
             revenue.save()
-            print(revenue.id)
-            print(revenue.money)
-            print(revenue.datetime)
-            print(Revenue.objects.aggregate(Sum('money')))
