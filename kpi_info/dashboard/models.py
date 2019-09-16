248ab91c9ce54d594ba98dae0bd28033855f0abf
Khue Doan <khuedoan98@gmail.com>
2019-09-16 11:22:16 +0700

    Revert "Remove dynamic files"
    This reverts commit d4f441cb667c3cb6f6c4be9ac4b7d57c071d43d6.

--- kpi_info/dashboard/models.py
+++ kpi_info/dashboard/models.py
@@ -3,18 +3,15 @@ from django.db.models import Sum
 import datetime
 
 class Revenue(models.Model):
-    bill_id = models.BigIntegerField(unique=True)
-    server_index = models.IntegerField()
-    player_id = models.BigIntegerField()
-    player_name = models.BinaryField()
-    pay_money = models.FloatField()
-    order_time = models.DateTimeField()
+    id = models.CharField(max_length=100, primary_key=True, verbose_name='ID')
+    money = models.IntegerField()
+    datetime = models.DateTimeField()
 
     # Get total revenue in specific time range
     def get_from_range(self, start_time, end_time):
         transaction_in_range = Revenue.objects.filter(
             datetime__range = (start_time, end_time),
         ).annotate(
-            hourly_revenue = Sum('pay_money')
+            hourly_revenue = Sum('money')
         )
-        return sum(transaction.pay_money for transaction in transaction_in_range)
+        return sum(transaction.money for transaction in transaction_in_range)
