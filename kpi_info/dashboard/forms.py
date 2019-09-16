4727cdc20a4fc65c78f2e5aecc74ebb77bcae168
Khue Doan <khuedoan98@gmail.com>
2019-09-16 10:42:06 +0700

    Start fresh

--- kpi_info/dashboard/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class DateRangeForm(forms.Form):
-    start_date = forms.DateField(input_formats=[ '%Y-%m-%d' ], required=True)
-    end_date = forms.DateField(input_formats=[ '%Y-%m-%d' ], required=True)
