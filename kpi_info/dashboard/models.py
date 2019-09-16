17c6853d783a0b353df9b2401ef9198e8a30bbc9
Khue Doan <khuedoan98@gmail.com>
2019-09-16 10:39:11 +0700

    Prototype for new log format

--- kpi_info/dashboard/models.py
+++ kpi_info/dashboard/models.py
@@ -3,15 +3,18 @@ from django.db.models import Sum
 import datetime
 
 class Revenue(models.Model):
-    id = models.CharField(max_length=100, primary_key=True, verbose_name='ID')
-    money = models.IntegerField()
-    datetime = models.DateTimeField()
+    bill_id = models.BigIntegerField(unique=True)
+    server_index = models.IntegerField()
+    player_id = models.BigIntegerField()
+    player_name = models.BinaryField()
+    pay_money = models.FloatField()
+    order_time = models.DateTimeField()
 
     # Get total revenue in specific time range
     def get_from_range(self, start_time, end_time):
         transaction_in_range = Revenue.objects.filter(
             datetime__range = (start_time, end_time),
         ).annotate(
-            hourly_revenue = Sum('money')
+            hourly_revenue = Sum('pay_money')
         )
-        return sum(transaction.money for transaction in transaction_in_range)
+        return sum(transaction.pay_money for transaction in transaction_in_range)
